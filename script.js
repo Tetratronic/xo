d5df3528f73a76c70701382523888e7dfdb20eeb
aymane <aymanebk@gmail.com>
2023-11-16 14:28:50 +0100

    working game using encapsulation

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-const Board = function (name) {
-    const boardType = name;
+const gameBoard = (function () {
     const cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
     const printBoard = () => console.log("|%s|%s|%s|\n|%s|%s|%s|\n|%s|%s|%s|\n", cells[0], cells[1],cells[2],cells[3],cells[4],cells[5],cells[6],cells[7],cells[8]);
     const populateBoard = (player, cell) => {      
           if(cells[cell] != " "){
-            return -1;2
+            return -1;
 
           }
         player === 1 ? cells[cell] = 'X' : cells[cell] = 'O';
@@ -21,31 +20,81 @@ const Board = function (name) {
         }
         return false;
     }
-    return {boardType, printBoard, populateBoard, checkWin};
-}
-
-const gameBoard = Board("testboard");
+    return {printBoard, populateBoard, checkWin};
+})();
 
 //Game Loop;
-function loop(){
-let count = 0;
-let player = 1;
-gameBoard.printBoard();
-while (count < 9){
-        let input = parseInt(prompt("Choose a cell")) - 1;
+// function loop(){
+// let count = 0;
+// let player = 1;
+// gameBoard.printBoard();
+// while (count < 9){
+//         let input = parseInt(prompt("Choose a cell")) - 1;
 
-        while ((input < 0 || input > 8) || gameBoard.populateBoard(player, input) == -1){
-            input = prompt("choose again") - 1;
-    }
-    console.clear();
-    gameBoard.printBoard();
-    if (gameBoard.checkWin()){
-        console.log("Player: %s WON !!", player);
-        return;
-    }
-    player = (player % 2) + 1;
-    count++;
-}
-}
+//         while ((input < 0 || input > 8) || gameBoard.populateBoard(player, input) == -1){
+//             input = prompt("choose again") - 1;
+//     }
+//     console.clear();
+//     gameBoard.printBoard();
+//     if (gameBoard.checkWin()){
+//         console.log("Player: %s WON !!", player);
+//         return;
+//     }
+//     player = (player % 2) + 1;
+//     count++;
+// }
+// }
 
-loop()
+// loop()
+
+
+const gameController = (function(){
+    playerOneName = "Player1";
+    playerTwoName = "Player2";
+
+    const players = [
+        {
+            name: playerOneName,
+            mark: 1
+        },
+        {
+            name: playerTwoName,
+            mark: 2
+        }
+    ];
+
+    let activePlayer = players[0];
+
+    const switchTurn = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    }
+
+    const getActivePlayer = () => {
+        activePlayer;
+    }
+
+    const printNewBoard = () => {
+        console.clear();
+        gameBoard.printBoard();
+        console.log("%s's turn !", activePlayer.name);
+    }
+
+    const playRound = (cell) =>{
+        if(gameBoard.populateBoard(activePlayer.mark,cell) === -1){
+            return;
+        }
+
+        if(gameBoard.checkWin()){
+            printNewBoard();
+            console.log("%s WON !", activePlayer.name);
+            return;
+        }
+
+        switchTurn();
+        printNewBoard();
+    }
+
+    printNewBoard();
+
+    return{playRound};
+})();
